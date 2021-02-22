b97050335a56a2a31ce633554ac66514a690134c
nacerillo <nicholascerillo@gmail.com>
2021-02-22 15:34:36 -0800

    completed getting of location and weather data

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 // ============== Packages ==============================
 
 const express = require('express');
-const cors = require('cors'); // just kinda works and we need it
-// If this line of code comes, delete it const { response } = require('express');
-require('dotenv').config(); // read the `.env` file's saved env variables AFTER reading the terminal's real env's variables
+const cors = require('cors'); 
+require('dotenv').config(); 
 
 
 // ============== App ===================================
 
-const app = express(); // express() will return a fully ready to run server object
-app.use(cors()); // enables local processes to talk to the server // Cross Origin Resource Sharing
+const app = express();
 
-const PORT = process.env.PORT || 3000; // process.env is boilerplace the variable name is potato
+const PORT = process.env.PORT || 3000;
 
 
 // ============== Routes ================================
@@ -19,33 +17,39 @@ const PORT = process.env.PORT || 3000; // process.env is boilerplace the variabl
 
   
   app.get('/location', handleGetLocation);
+  app.get('/weather',handleGetWeather);
   function handleGetLocation(req, res){
-    // console.log(req, res);
-    console.log(req.query); // {city: seattle} /// req.query.city : seattle
-    const dataFromTheFile = require('./data/location.json'); // in an express server, we can synchronously get data from a local json file without a .then
+    console.log(req.query); 
+    const dataFromTheFile = require('./data/location.json'); 
     const output = new Location(dataFromTheFile, req.query.city);
+    res.send(output);
+  }
+ 
+  app.get('/weather', handleGetWeather);
+  function handleGetWeather(req, res){
+
+    console.log(req.query);
+    const dataFromTheFile = require('./data/weather.json'); 
+    const output = []; 
+    for(var i = 0; i < dataFromTheFile.data.length; i++){
+        output.push(new Weather(dataFromTheFile,i));
+    }
     res.send(output);
 
   }
- 
-  /*app.get('/location', handleGetWeather);
-  function handleGetWeather(req, res){
-    // console.log(req, res);
-    console.log(req.query); // {city: seattle} /// req.query.city : seattle
-    const dataFromTheFile = require('./data/weather.json'); // in an express server, we can synchronously get data from a local json file without a .then
-    const output = new Weather(dataFromTheFile, req.query.city);
-    res.send(output);
-
-  }*/
 
 
-  function Location(dataFromTheFile, cityName){
-    this.search_query = cityName;
+  function Location(dataFromTheFile, locationName){
+    this.search_query = locationName;
     this.formatted_query = dataFromTheFile[0].display_name;
     this.latitude = dataFromTheFile[0].lat;
     this.longitude = dataFromTheFile[0].lon;
   }
 
+  function Weather(dataFromTheFile, index){
+    this.time = dataFromTheFile.data[index].valid_date;
+    this.forecast = dataFromTheFile.data[index].weather.description;
+  }
 
   app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`)); 
   
